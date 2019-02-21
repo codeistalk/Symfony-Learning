400d01e14a2a9f8440254f0ce1fbc9ef256f66c8
codeistalk <codeistalk@gmail.com>
2019-02-21 10:28:33 +0000

    added template rendering

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class LuckyController
  * @package App\Controller
  */
-class LuckyController {
+class LuckyController extends AbstractController {
 
 	/**
 	 * @Route("/lucky/number")
@@ -25,9 +26,9 @@ class LuckyController {
 	public function number () {
 		$number = random_int ( 0, 100 );
 
-		return new Response(
-			'<html lang="en"><body>Lucky number: ' . $number . '</body></html>'
-		);
+		return $this->render ( 'lucky/number.html.twig', [
+			'number' => $number,
+		] );
 	}
 
 }
