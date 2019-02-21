19f796747a36bc1e7c76bfa39a1ce197028ad649
codeistalk <codeistalk@gmail.com>
2019-02-21 14:11:04 +0000

    twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,13 +9,14 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ArticleController
  * @package App\Controller
  */
-class ArticleController {
+class ArticleController extends AbstractController {
 
 	/**
 	 * @Route("/")
@@ -29,6 +30,15 @@ class ArticleController {
 	 * @Route("/news/{slug}")
 	 */
 	public function show ( $slug ) {
-		return new Response( sprintf ( 'Future page to show the article: %s', $slug ) );
+		$comments = [
+			'Morbi condimentum justo ex, at.',
+			'eque porro quisquam est qui dolorem ipsum quia dolor sit amet',
+			'last one',
+		];
+
+		return $this->render ( 'article/show.html.twig', [
+			'title'    => ucwords ( str_replace ( '-', ' ', $slug ) ),
+			'comments' => $comments,
+		] );
 	}
 }
