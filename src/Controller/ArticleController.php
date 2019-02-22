b0d19c1cf318dfc445d213b178f69585351f0209
codeistalk <codeistalk@gmail.com>
2019-02-22 10:06:00 +0000

    added cache service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,7 @@ namespace App\Controller;
 
 use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -36,7 +37,7 @@ class ArticleController extends AbstractController {
 	 *
 	 * @return Response
 	 */
-	public function show ( $slug, MarkdownInterface $markdown ) {
+	public function show ( $slug, MarkdownInterface $markdown, AdapterInterface $cache ) {
 		$comments = [
 			'Morbi condimentum justo ex, at.',
 			'eque porro quisquam est qui dolorem ipsum quia dolor sit amet',
@@ -44,7 +45,7 @@ class ArticleController extends AbstractController {
 		];
 
 		$artcleContent = <<<EOF
-Spicy **jalapeno** bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
+Spicy **jalapeno** bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow **turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
 
@@ -55,7 +56,13 @@ Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin b
 Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui beef ribs dolore pariatur. Minim strip steak fugiat nisi est, meatloaf pig aute. Swine rump turducken nulla sausage. Reprehenderit pork belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod. Doner drumstick short loin, adipisicing cow cillum tenderloin.
 EOF;
 
-		$artcleContent = $markdown->transform ( $artcleContent );
+		$item = $cache->getItem ( 'markdown_' . md5 ( $artcleContent ) );
+		if ( !$item->isHit () ) {
+			$item->set ( $markdown->transform ( $artcleContent ) );
+			$cache->save ( $item );
+		}
+
+		$artcleContent = $item->get ();
 
 		return $this->render ( 'article/show.html.twig', [
 			'title'          => ucwords ( str_replace ( '-', ' ', $slug ) ),
