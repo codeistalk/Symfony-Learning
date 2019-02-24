d4c73544786f62437fa0e0a1cbad8fcf13d9c3bc
napestershine <napestershine@gmail.com>
2019-02-24 16:17:22 +0100

    added custom fixture

---
+++ src/DataFixtures/BaseFixture.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radhasoami
+ * Date: 2/24/2019
+ * Time: 4:04 PM
+ */
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+abstract class BaseFixture extends Fixture
+{
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    abstract protected function loadData(ObjectManager $em);
+
+    public function load(ObjectManager $manager)
+    {
+
+        $this->manager = $manager;
+
+        $this->loadData($manager);
+    }
+
+
+    protected function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = new $className();
+            $factory($entity, $i);
+
+            $this->manager->persist($entity);
+
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+}
