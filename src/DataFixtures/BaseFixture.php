af9b835f71fae05daf11f897fcc4d09d6a6027ef
codeistalk <codeistalk@gmail.com>
2019-02-25 12:26:45 +0000

    user entity

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: radhasoami
- * Date: 2/24/2019
- * Time: 4:04 PM
- */
 
 namespace App\DataFixtures;
 
@@ -13,87 +7,85 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
 
-abstract class BaseFixture extends Fixture {
-	/**
-	 * @var ObjectManager
-	 */
-	private $manager;
+abstract class BaseFixture extends Fixture
+{
+    /** @var ObjectManager */
+    private $manager;
 
-	/**
-	 * @var Generator
-	 */
-	protected $faker;
+    /** @var Generator */
+    protected $faker;
 
-	private $referencesIndex = [];
+    private $referencesIndex = [];
 
-	abstract protected function loadData ( ObjectManager $em );
+    abstract protected function loadData(ObjectManager $manager);
 
-	public function load ( ObjectManager $manager ) {
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+        $this->faker = Factory::create();
 
-		$this->manager = $manager;
-		$this->faker = Factory::create ();
+        $this->loadData($manager);
+    }
 
-		$this->loadData ( $manager );
-	}
+    /**
+     * Create many objects at once:
+     *
+     *      $this->createMany(10, function(int $i) {
+     *          $user = new User();
+     *          $user->setFirstName('Ryan');
+     *
+     *           return $user;
+     *      });
+     *
+     * @param int      $count
+     * @param string   $groupName Tag these created objects with this group name,
+     *                            and use this later with getRandomReference(s)
+     *                            to fetch only from this specific group.
+     * @param callable $factory
+     */
+    protected function createMany(int $count, string $groupName, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = $factory($i);
 
-	/**
-	 * Create many objects at once:
-	 *
-	 *      $this->createMany(10, function(int $i) {
-	 *          $user = new User();
-	 *          $user->setFirstName('Ryan');
-	 *
-	 *           return $user;
-	 *      });
-	 *
-	 * @param int      $count
-	 * @param string   $groupName Tag these created objects with this group name,
-	 *                            and use this later with getRandomReference(s)
-	 *                            to fetch only from this specific group.
-	 * @param callable $factory
-	 */
-	protected function createMany ( int $count, string $groupName, callable $factory ) {
-		for ( $i = 0; $i < $count; $i++ ) {
-			$entity = $factory( $i );
+            if (null === $entity) {
+                throw new \LogicException('Did you forget to return the entity object from your callback to BaseFixture::createMany()?');
+            }
 
-			if ( null === $entity ) {
-				throw new \LogicException( 'Did you forget to return the entity object from your callback to BaseFixture::createMany()?' );
-			}
+            $this->manager->persist($entity);
 
-			$this->manager->persist ( $entity );
+            // store for usage later as groupName_#COUNT#
+            $this->addReference(sprintf('%s_%d', $groupName, $i), $entity);
+        }
+    }
 
-			// store for usage later as groupName_#COUNT#
-			$this->addReference ( sprintf ( '%s_%d', $groupName, $i ), $entity );
-		}
-	}
+    protected function getRandomReference(string $groupName) {
+        if (!isset($this->referencesIndex[$groupName])) {
+            $this->referencesIndex[$groupName] = [];
 
-	protected function getRandomReference ( string $groupName ) {
-		if ( !isset( $this->referencesIndex[$groupName] ) ) {
-			$this->referencesIndex[$groupName] = [];
+            foreach ($this->referenceRepository->getReferences() as $key => $ref) {
+                if (strpos($key, $groupName.'_') === 0) {
+                    $this->referencesIndex[$groupName][] = $key;
+                }
+            }
+        }
 
-			foreach ( $this->referenceRepository->getReferences () as $key => $ref ) {
-				if ( strpos ( $key, $groupName . '_' ) === 0 ) {
-					$this->referencesIndex[$groupName][] = $key;
-				}
-			}
-		}
+        if (empty($this->referencesIndex[$groupName])) {
+            throw new \InvalidArgumentException(sprintf('Did not find any references saved with the group name "%s"', $groupName));
+        }
 
-		if ( empty( $this->referencesIndex[$groupName] ) ) {
-			throw new \InvalidArgumentException( sprintf ( 'Did not find any references saved with the group name "%s"', $groupName ) );
-		}
+        $randomReferenceKey = $this->faker->randomElement($this->referencesIndex[$groupName]);
 
-		$randomReferencekey = $this->faker->randomElement ( $this->referencesIndex[$groupName] );
+        return $this->getReference($randomReferenceKey);
+    }
 
-		return $this->getReference ( $randomReferencekey );
-	}
+    protected function getRandomReferences(string $className, int $count)
+    {
+        $references = [];
+        while (count($references) < $count) {
+            $references[] = $this->getRandomReference($className);
+        }
 
-	protected function getRandomReferences ( string $className, int $count ) {
-		$references = [];
-
-		while ( count ( $references ) < $count ) {
-			$references[] = $this->getRandomReference ( $className );
-		}
-
-		return $references;
-	}
+        return $references;
+    }
 }
--- src/DataFixtures/TagFixture.php
+++ src/DataFixtures/TagFixture.php
@@ -5,12 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\Tag;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class TagFixture extends BaseFixture {
-	protected function loadData ( ObjectManager $manager ) {
-		$this->createMany ( Tag::class, 10, function ( Tag $tag ) {
-			$tag->setName ( $this->faker->realText ( 20 ) );
-		} );
+class TagFixture extends BaseFixture
+{
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(10, 'main_tags', function() {
+            $tag = new Tag();
+            $tag->setName($this->faker->realText(20));
 
-		$manager->flush ();
-	}
+            return $tag;
+        });
+
+        $manager->flush();
+    }
 }
