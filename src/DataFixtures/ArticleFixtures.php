af907101d0c70a9cb8a31935250a0dccb7462c8a
codeistalk <codeistalk@gmail.com>
2019-02-25 07:17:59 +0000

    added comment fixture

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,37 +2,35 @@
 
 namespace App\DataFixtures;
 
-
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
-{
-    private static $articleTitles = [
-        'Why Asteroids Taste Like Bacon',
-        'Life on Planet Mercury: Tan, Relaxing and Fabulous',
-        'Light Speed Travel: Fountain of Youth of Fallacy'
-    ];
+class ArticleFixtures extends BaseFixture {
+	private static $articleTitles = [
+		'Why Asteroids Taste Like Bacon',
+		'Life on Planet Mercury: Tan, Relaxing and Fabulous',
+		'Light Speed Travel: Fountain of Youth of Fallacy',
+	];
 
-    private static $articleImages = [
-        'asteroid.jpeg',
-        'mercury.jpeg',
-        'lightspeed.png',
-    ];
+	private static $articleImages = [
+		'asteroid.jpeg',
+		'mercury.jpeg',
+		'lightspeed.png',
+	];
 
-    private static $articleAuthors = [
-        'Mike Ferengi',
-        'Amy Oort'
-    ];
+	private static $articleAuthors = [
+		'Mike Ferengi',
+		'Amy Oort',
+	];
 
-    protected function loadData(ObjectManager $manager)
-    {
+	protected function loadData ( ObjectManager $manager ) {
 
-        $this->createMany(Article::class, 10, function (Article $article, $count) {
+		$this->createMany ( Article::class, 10, function ( Article $article, $count ) use ( $manager ) {
 
-            $article->setTitle($this->faker->randomElement(self::$articleTitles))
-                ->setContent(
-                    <<<EOF
+			$article->setTitle ( $this->faker->randomElement ( self::$articleTitles ) )
+			        ->setContent (
+				        <<<EOF
 Spicy **jalapeno** bacon ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow **turkey** shank eu pork belly meatball non cupim.
 
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
@@ -43,18 +41,30 @@ Sausage tenderloin officia jerky nostrud. Laborum elit pastrami non, pig kevin b
 
 Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui beef ribs dolore pariatur. Minim strip steak fugiat nisi est, meatloaf pig aute. Swine rump turducken nulla sausage. Reprehenderit pork belly tongue alcatra, shoulder excepteur in beef bresaola duis ham bacon eiusmod. Doner drumstick short loin, adipisicing cow cillum tenderloin.
 EOF
-                );
+			        );
 
-            // Publish most articles
-            if ($this->faker->boolean(70)) {
-                $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
-            }
+			// Publish most articles
+			if ( $this->faker->boolean ( 70 ) ) {
+				$article->setPublishedAt ( $this->faker->dateTimeBetween ( '-100 days', '-1 days' ) );
+			}
 
-            $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
-                ->setHeartCount($this->faker->numberBetween(5, 100))
-                ->setImageFilename($this->faker->randomElement(self::$articleImages));
+			$article->setAuthor ( $this->faker->randomElement ( self::$articleAuthors ) )
+			        ->setHeartCount ( $this->faker->numberBetween ( 5, 100 ) )
+			        ->setImageFilename ( $this->faker->randomElement ( self::$articleImages ) );
 
-        });
-        $manager->flush();
-    }
+			$comment1 = new Comment();
+			$comment1->setAuthourName ( 'Mike Ferengi' )
+			         ->setContent ( 'I ate a normal rock once. it did NOT taste like bacon!' );
+			$comment1->setArticle ( $article );
+			$manager->persist ( $comment1 );
+
+			$comment2 = new Comment();
+			$comment2->setAuthourName ( 'Mike Ferengi' )
+			         ->setContent ( 'Woohoo! I\'m going on an all-asteroid diet!' );
+			$comment2->setArticle ( $article );
+			$manager->persist ( $comment2 );
+
+		} );
+		$manager->flush ();
+	}
 }
