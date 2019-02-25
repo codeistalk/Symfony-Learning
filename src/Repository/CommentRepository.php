b69334f9d9552ecdcdbd7da1da0737fd15812f28
codeistalk <codeistalk@gmail.com>
2019-02-25 09:35:17 +0000

    searching

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -7,44 +7,60 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 /**
- * @method Comment|null find($id, $lockMode = null, $lockVersion = null)
- * @method Comment|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Comment|null find( $id, $lockMode = null, $lockVersion = null )
+ * @method Comment|null findOneBy( array $criteria, array $orderBy = null )
  * @method Comment[]    findAll()
- * @method Comment[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Comment[]    findBy( array $criteria, array $orderBy = null, $limit = null, $offset = null )
  */
-class CommentRepository extends ServiceEntityRepository
-{
-    public function __construct(RegistryInterface $registry)
-    {
-        parent::__construct($registry, Comment::class);
-    }
+class CommentRepository extends ServiceEntityRepository {
+	public function __construct ( RegistryInterface $registry ) {
+		parent::__construct ( $registry, Comment::class );
+	}
 
-    // /**
-    //  * @return Comment[] Returns an array of Comment objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+	/**
+	 * @param string|null $term
+	 *
+	 * @return Comment[]
+	 */
+	public function findAllWithSearch ( ?string $term ) {
+		$qb = $this->createQueryBuilder ( 'c' );
 
-    /*
-    public function findOneBySomeField($value): ?Comment
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+		if ( $term ) {
+			$qb->andWhere ( 'c.content LIKE :term OR c.authourName LIKE :term' )
+			   ->setParameter ( 'term', '%' . $term . '%' );
+		}
+
+		return $qb->orderBy ( 'c.createdAt', 'DESC' )
+		          ->getQuery ()
+		          ->getResult ();
+	}
+
+	// /**
+	//  * @return Comment[] Returns an array of Comment objects
+	//  */
+	/*
+	public function findByExampleField($value)
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->orderBy('c.id', 'ASC')
+			->setMaxResults(10)
+			->getQuery()
+			->getResult()
+		;
+	}
+	*/
+
+	/*
+	public function findOneBySomeField($value): ?Comment
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->getQuery()
+			->getOneOrNullResult()
+		;
+	}
+	*/
 }
