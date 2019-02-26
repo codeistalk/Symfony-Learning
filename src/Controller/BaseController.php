eea40c5625546f1bc93b635247425a38ac598937
codeistalk <codeistalk@gmail.com>
2019-02-26 07:46:04 +0000

    account page

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manoj
+ * Date: 26.02.19
+ * Time: 08:37
+ */
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+abstract class BaseController extends AbstractController {
+	protected function getUser (): User {
+
+		return parent::getUser ();
+	}
+
+}
