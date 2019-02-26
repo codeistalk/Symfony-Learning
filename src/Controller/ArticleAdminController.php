1bb2bb46cea86635da5ca4027b0fedf95bb5d007
codeistalk <codeistalk@gmail.com>
2019-02-26 07:22:26 +0000

    roles

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -8,9 +8,9 @@
 
 namespace App\Controller;
 
-
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,23 +18,21 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class ArticleAdminController
  * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
  */
-class ArticleAdminController extends AbstractController
-{
+class ArticleAdminController extends AbstractController {
 
-    /**
-     * @Route("/admin/article/new")
-     */
-    public function new(EntityManagerInterface $em)
-    {
-        die('todo');
+	/**
+	 * @Route("/admin/article/new", name="admin_article_new")
+	 */
+	public function new ( EntityManagerInterface $em ) {
+		die( 'todo' );
 
-
-        return new Response(sprintf(
-            'Hiya! new article is: #%d slug: %s',
-            $article->getId(),
-            $article->getSlug()
-        ));
-    }
+		return new Response( sprintf (
+			                     'Hiya! new article is: #%d slug: %s',
+			                     $article->getId (),
+			                     $article->getSlug ()
+		                     ) );
+	}
 
 }
